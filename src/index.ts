95d6986efd4a40b6c765b13d88bb8dc0412c8d8e
Vincent Engel <ve@vesion.io>
2020-05-21 17:22:39 +0200

    Reformat src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,39 @@
 export interface IfTTTResponse {
-    status: number;
-    message: string;
+  status: number;
+  message: string;
+}
+
+export class IfTTTApi {
+  private readonly key: string;
+
+  public constructor(key: string) {
+    this.key = key;
   }
-  
-  export class IfTTTApi {
-    private readonly key: string;
-  
-    public constructor(key: string) {
-      this.key = key;
-    }
-  
-    public async trigger(
-      event: string,
-      value1?: string,
-      value2?: string,
-      value3?: string,
-    ): Promise<IfTTTResponse> {
-      const response = await fetch(
-        `https://maker.ifttt.com/trigger/${event}/with/key/${this.key}`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            value1,
-            value2,
-            value3,
-          }),
+
+  public async trigger(
+    event: string,
+    value1?: string,
+    value2?: string,
+    value3?: string,
+  ): Promise<IfTTTResponse> {
+    const response = await fetch(
+      `https://maker.ifttt.com/trigger/${event}/with/key/${this.key}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
-  
-      return {
-        status: response.status,
-        message: await response.text(),
-      };
-    }
+        body: JSON.stringify({
+          value1,
+          value2,
+          value3,
+        }),
+      },
+    );
+
+    return {
+      status: response.status,
+      message: await response.text(),
+    };
   }
-  
+}
